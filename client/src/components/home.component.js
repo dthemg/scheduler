aecce781d6f1a6bc15df9a3df5cb1b38de9d95f8
dthemg <david.montgomery@live.se>
2020-05-14 23:35:17 +0200

    Decoding JWT's

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 export default function Home() {
 	const item = localStorage.getItem('jwtToken');
+	const decodedItem = jwt_decode(item);
 
 	return (
 		<div>
 			<h1>Home</h1>
-			<p>JWT TOKEN</p>
+			<h2>Jwt token</h2>
 			<p>{item}</p>
+			<h2>Decoded JWT</h2>
+			<p>Id: {decodedItem.id}</p>
+			<p>Logged-in user name: {decodedItem.name}</p>
+			<p>Expires in: {decodedItem.exp / (1000 * 60 * 60)} hours</p>
 		</div>
 	);
 }
