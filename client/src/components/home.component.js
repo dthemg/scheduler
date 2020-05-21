17befe2f72f352a99fc8d46ccb959476739b47ba
dthemg <david.montgomery@live.se>
2020-05-22 00:39:57 +0200

    Looks nicer now

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import jwt_decode from 'jwt-decode';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -30,12 +29,17 @@ const useStyles = makeStyles((theme) => ({
 
 function AppointmentCard(props) {
 	const classes = useStyles();
+
+	var hrsAndMins = props.time.split('T')[1].split(':');
+	var hrs = hrsAndMins[0];
+	var mins = hrsAndMins[1];
+
 	return (
 		<Card className={classes.card}>
 			<CardActionArea>
 				<CardContent>
 					<Typography gutterBottom variant='h5' component='h2'>
-						{props.time}
+						{hrs} : {mins}
 					</Typography>
 				</CardContent>
 			</CardActionArea>
@@ -43,18 +47,16 @@ function AppointmentCard(props) {
 	);
 }
 AppointmentCard.propTypes = {
-	time: PropTypes.instanceOf(Date),
+	time: PropTypes.string,
 };
 
 export default function Home(props) {
 	const [appointments, setAppointments] = useState([]);
-
-	const item = localStorage.getItem('jwtToken');
 	const classes = useStyles();
 
 	useEffect(() => {
 		console.log('UseEffect called');
-		if (item) {
+		if (localStorage.getItem('jwtToken')) {
 			axios({
 				method: 'get',
 				url: 'http://localhost:9000/data/getAllCalendarDates',
@@ -70,17 +72,43 @@ export default function Home(props) {
 		}
 	}, []);
 
+	const getDate = (dateString) => {
+		return dateString.split('T')[0];
+	};
+
+	const addDayHeader = (idx, arr) => {
+		// If first element add header
+		if (idx == 0) {
+			return true;
+		}
+
+		if (getDate(arr[idx - 1].time) !== getDate(arr[idx].time)) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
 	return (
 		<div>
-			<div className={classes.root}>
-				{props.loggedIn ? (
-					appointments.map((val, idx) => {
-						return <AppointmentCard key={idx} time={val.time} />;
-					})
-				) : (
-					<h2>Log in to see available times</h2>
-				)}
-			</div>
+			{props.loggedIn ? (
+				appointments.map((val, idx, arr) => {
+					return (
+						<div key={idx}>
+							{addDayHeader(idx, arr) ? (
+								<h1>{getDate(val.time)}</h1>
+							) : (
+								<h1></h1>
+							)}
+							<div className={classes.root}>
+								<AppointmentCard time={val.time} />
+							</div>
+						</div>
+					);
+				})
+			) : (
+				<h2>Log in to see available times</h2>
+			)}
 		</div>
 	);
 }
